eb181940f87c74d9b9789e6161752748397dba80
Dan Reuther <dreuther@catalystitservices.com>
2015-10-29 14:06:30 -0700

    Add base tests

---
+++ src/test/java/com/catalyst/collector/daos/hibernate/TestDataDaoHibernateTest.java
@@ -0,0 +1,14 @@
+package com.catalyst.collector.daos.hibernate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDataDaoHibernateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/catalyst/collector/services/impl/TestDataServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.catalyst.collector.services.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDataServiceImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
