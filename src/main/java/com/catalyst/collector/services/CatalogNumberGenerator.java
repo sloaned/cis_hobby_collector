a33b8972dc73c40573aae7e4e2ddf2b1255545b9
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-11-04 15:17:27 -0800

    Finished Generator

---
+++ src/main/java/com/catalyst/collector/services/CatalogNumberGenerator.java
@@ -0,0 +1,22 @@
+package com.catalyst.collector.services;
+
+/**
+ * Created by gstringfellow on 11/4/2015.
+ */
+
+import java.io.Serializable;
+
+import com.catalyst.collector.entities.Collectible;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.SequenceGenerator;
+public class CatalogNumberGenerator
+        extends SequenceGenerator
+{
+    @Override
+    public Serializable generate(SessionImplementor session, Object obj)
+    {
+        Collectible c =(Collectible) obj;
+        // generate your custom sequence here
+        return c.getName().substring(0,2)+c.hashCode(); // In this example, return an int
+    }
+}
