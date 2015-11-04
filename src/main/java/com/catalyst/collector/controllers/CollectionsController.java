e647b2825510035ad14124ca4036bdb4267bdf8b
Dan Sloane <dsloane@catalystdevworks.com>
2015-11-04 13:40:13 -0800

    fixed all merge errors

--- src/main/java/com/catalyst/collector/controllers/CollectionsController.java
+++ src/main/java/com/catalyst/collector/controllers/CollectionsController.java
@@ -1,15 +1,18 @@
 package com.catalyst.collector.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.catalyst.collector.entities.Category;
+import com.catalyst.collector.entities.Color;
 import com.catalyst.collector.services.CollectionsService;
 
 @RestController
@@ -41,6 +44,23 @@ public class CollectionsController {
 	@RequestMapping(value="/category/{id}", method=RequestMethod.DELETE)
 	public void deleteCategory(@PathVariable Integer id){
 		collectionsService.deleteCategory(id);
+	}
+	
+	@RequestMapping(value="/color", method=RequestMethod.GET)
+	public List<Color> getColorList() {
+		return collectionsService.getColorList();
+	}
+	@RequestMapping(value="/color{colorType}", method=RequestMethod.DELETE)
+	public boolean removeColor(@PathVariable String colorType) {
+		return collectionsService.removeColor(colorType);
+	}
+	@RequestMapping(value="/color", method=RequestMethod.POST)
+	public void addColor(@RequestBody Color c) {
+		collectionsService.addColor(c);
+	}
+	@RequestMapping(value="/color", method=RequestMethod.PUT)
+	public boolean updateColor(@PathVariable int id, @RequestParam(value="color",required=true)String color) {
+		return collectionsService.updateColor(id, color);
 	}
 	
 	
