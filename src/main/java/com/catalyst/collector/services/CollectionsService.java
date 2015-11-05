532de4983c39f65870d51b9400be1172d54708a6
Joshua Jacobson <jjacobson@catalystitservices.com>
2015-11-05 11:22:35 -0800

    Fixed merge conflict

--- src/main/java/com/catalyst/collector/services/CollectionsService.java
+++ src/main/java/com/catalyst/collector/services/CollectionsService.java
@@ -1,13 +1,80 @@
 package com.catalyst.collector.services;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.catalyst.collector.entities.Keyword;
 
 public interface CollectionsService {
 
+	/**
+	 * gets every category object
+	 * @return ArrayList of all categories
+	 */
+	public ArrayList<Category> getCategory();
+
+	/**
+	 * adds a new category to the database
+	 * @param category
+	 */
+	public boolean addCategory(Category category);
+
+	/**
+	 * updates the given category (which will be identified by the id number)
+	 * @param id, category
+	 */
+	public boolean updateCategory(int id, Category category);
+
+	/**
+	 * deletes the category which matches the given id number
+	 * @param id
+	 */
+	public boolean deleteCategory(int id);
+
+	ArrayList<Collectible> getCollectibles();
+
+	Collectible getCollectible(Integer id);
+
+	public Color getByColorId(int colorId);
+	public List<Color> getColorList();
+	public boolean addColor(Color addedColor);
+	public boolean removeColor(int id);
+	public boolean updateColor(int id, String color);
+	public Color getColor(int colorId);
+    ArrayList<Condition> getAllConditions();
+    boolean addCondition(Condition condition);
+    boolean updateCondition(Condition condition);
+    void deleteCondition(Integer id);
     public ArrayList<Keyword> getAllKeywords();
     public boolean addKeyword(Keyword keyword);
     public boolean updateKeyword(Keyword keyword);
     public void removeKeyword(Integer id);
+
+
+	/**
+	 * Calls the add Age method in the Dao
+	 * @param age
+	 */
+	public void addAge(Age age);
+
+	/**
+	 * Calls the get age types method in the Dao
+	 * @return
+	 */
+	ArrayList<Age> getAgeTypes();
+
+	/**
+	 * Calls the update age method in the Dao
+	 * @param age
+	 */
+	void updateAge(Age age);
+
+	/**
+	 * Calls the delete age method in the Dao
+	 * @param id
+	 */
+	void deleteAge(Integer id);
+
+
+
 }
--- src/main/java/com/catalyst/collector/services/impl/CollectionsServiceImpl.java
+++ src/main/java/com/catalyst/collector/services/impl/CollectionsServiceImpl.java
@@ -21,6 +21,102 @@ public class CollectionsServiceImpl implements CollectionsService {
 	}
 
 
+	@Override
+	public void addAge(Age age) {
+		collectionsDao.addAge(age);
+	}
+
+	@Override
+	public ArrayList<Age> getAgeTypes(){
+		return collectionsDao.getAgeTypes();
+	}
+
+	@Override
+	public void updateAge(Age age){
+		collectionsDao.updateAge(age);
+	}
+
+	@Override
+	public void deleteAge(Integer id) {
+		collectionsDao.deleteAge(id);
+	}
+	public ArrayList<Category> getCategory() {
+
+		return collectionsDao.getCategory();
+	}
+
+
+	@Override
+	public boolean addCategory(Category category) {
+		return collectionsDao.addCategory(category);
+
+	}
+
+
+	@Override
+	public boolean updateCategory(int id, Category category) {
+		category.setId(id);
+		return collectionsDao.updateCategory(category);
+	}
+
+	@Override
+	public boolean deleteCategory(int id) {
+		return collectionsDao.deleteCategory(id);
+	}
+
+	@Override
+	public List<Color> getColorList() {
+		return collectionsDao.getColorList();
+	}
+	@Override
+	public boolean addColor(Color addedColor) {
+		try{
+			collectionsDao.addColor(addedColor);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+		}
+	@Override
+	public boolean removeColor(int id) {
+		try{
+		collectionsDao.removeColor(id);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean updateColor(int id, String color){
+		try{
+		Color c = getColor(id);
+		c.setColor(color);
+		collectionsDao.updateColor(c);
+		}
+		catch(Exception e){
+			return false;
+			}
+		return true;
+	}
+	public Color getColor(int id){
+		List<Color> colors = getColorList();
+		for(Color c: colors){
+			if(c.getId() == id){
+				return c;
+			}
+		}
+		return null;
+
+	}
+
+	@Override
+	public Color getByColorId(int colorId){
+		return collectionsDao.getColor(colorId);
+	}
+
     @Override
     public ArrayList<Keyword> getAllKeywords() {
         return collectionsDao.getAllKeywords();
