41af5b4b9bbb67596275f08ead7c30cfbc99a721
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-11-13 14:35:24 -0800

    fixed bug where if you clicked outside of the catalogNUmber input field than all input fields would be cleared

--- src/main/resources/public/js/update.js
+++ src/main/resources/public/js/update.js
@@ -1,10 +1,13 @@
 function update(){
-//set self to the row of the CatalogNumber clicked
+    //if already changed td to input then don't do it again again
+    if($(this).children().hasClass("editField"))return;
+    //set self to the row of the CatalogNumber clicked
     self = $(this.parentNode);
     //console.log(self)
+
     //change all td to input boxes
-    self.children().each(function(td){
+    self.children().each(function(){
         text = $(this).text();
-        $(this).html("<input class=\"input\"type=\"text\" value=\""+ text +"\">")
+        $(this).html("<input class=\"editField\"type=\"text\" value=\""+ text +"\">");
     });
 }
