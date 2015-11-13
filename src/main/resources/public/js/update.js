564e47c93f3158bfc9e58e1b06ccfed1c1a37cc7
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-11-13 15:14:01 -0800

    Added submit button  functionaliy

--- src/main/resources/public/js/update.js
+++ src/main/resources/public/js/update.js
@@ -7,13 +7,19 @@ function update(){
 
     //change all td to input boxes
     row.children().each(function(){
+        //set this to what this is (the td)
+        td=this;
         //if this the submit button we don't want to change it to a input field but we want to show it
-        if($(this).children().hasClass("editSubmitButton")){
-            $(this).children().toggleClass("hidden");
+
+        if($(td).children().hasClass("editSubmitButton")){
+            $(td).children().toggleClass("hidden");
             return;
         }
-
-        text = $(this).text();
-        $(this).html("<input class=\"editField\"type=\"text\" value=\""+ text +"\">");
+        text = $(td).text();
+        $(td).html("<input class=\"editField\"type=\"text\" value=\""+ text +"\">");
     });
+    $(".editSubmitButton").click(updated)
+}
+function updated(){
+    console.log("hi")
 }
