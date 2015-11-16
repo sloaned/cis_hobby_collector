850cd4dd0c587c815f353f3c0033ddb96156539c
derek.e.delaney@gmail.com <Derek.e.delaney@gmail.com>
2015-11-16 09:35:08 -0800

    added check for os version for linux in selenium settings

--- src/main/java/com/catalyst/collector/SeleniumFramework/SeleniumSettings.java
+++ src/main/java/com/catalyst/collector/SeleniumFramework/SeleniumSettings.java
@@ -13,13 +13,15 @@ public class SeleniumSettings {
 
     private String CHROME_MAC = "src/test/java/com/catalyst/collector/PageObjectFramework/Drivers/chromedriver";
     private String CHROME_WIN = "src/test/java/com/catalyst/collector/PageObjectFramework/Drivers/chromedriver.exe";
+    private String CHROME_LNX = "src/test/java/com/catalyst/collector/PageObjectFramework/Drivers/chromedriverlinux";
 
     public SeleniumSettings(){
         if (SystemUtils.IS_OS_MAC)
             System.setProperty("webdriver.chrome.driver", CHROME_MAC);
         if (SystemUtils.IS_OS_WINDOWS)
             System.setProperty("webdriver.chrome.driver", CHROME_WIN);
-        //ToDo: add driver for linux
+        if (SystemUtils.IS_OS_LINUX)
+            System.setProperty("webdriver.chrome.driver", CHROME_LNX);
 
         driver = new ChromeDriver();
     }
