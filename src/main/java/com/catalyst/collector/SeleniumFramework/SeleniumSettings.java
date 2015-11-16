fa49650c26fa6d861899992c659c5f9186e23355
derek.e.delaney@gmail.com <Derek.e.delaney@gmail.com>
2015-11-16 09:27:41 -0800

    added check for os version for mac and windows in selenium settings

--- src/main/java/com/catalyst/collector/SeleniumFramework/SeleniumSettings.java
+++ src/main/java/com/catalyst/collector/SeleniumFramework/SeleniumSettings.java
@@ -1,5 +1,6 @@
 package com.catalyst.collector.SeleniumFramework;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,8 +11,16 @@ public class SeleniumSettings {
 
     private WebDriver driver;
 
+    private String CHROME_MAC = "src/test/java/com/catalyst/collector/PageObjectFramework/Drivers/chromedriver";
+    private String CHROME_WIN = "src/test/java/com/catalyst/collector/PageObjectFramework/Drivers/chromedriver.exe";
+
     public SeleniumSettings(){
-        System.setProperty("webdriver.chrome.driver", "src/test/java/com/catalyst/collector/PageObjectFramework/Drivers/chromedriver");
+        if (SystemUtils.IS_OS_MAC)
+            System.setProperty("webdriver.chrome.driver", CHROME_MAC);
+        if (SystemUtils.IS_OS_WINDOWS)
+            System.setProperty("webdriver.chrome.driver", CHROME_WIN);
+        //ToDo: add driver for linux
+
         driver = new ChromeDriver();
     }
 
