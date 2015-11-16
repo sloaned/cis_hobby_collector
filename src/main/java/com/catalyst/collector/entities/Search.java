d668f7a50e2dbd67597da44917a9f2b128aea963
Greg Fisher <gfisher@catalystitservices.com>
2015-11-16 09:35:47 -0800

    created a search entity

---
+++ src/main/java/com/catalyst/collector/entities/Search.java
@@ -0,0 +1,27 @@
+package com.catalyst.collector.entities;
+
+import java.util.Set;
+
+/**
+ * Created by gfisher on 11/16/2015.
+ */
+public class Search {
+
+    private Category category;
+
+    private String catalogueNumber;
+
+    private String name;
+
+    private String description;
+
+    private Age age;
+
+    private Condition condition;
+
+    private Color color;
+
+    private Set<Keyword> keywords;
+
+    private boolean sold;
+}
