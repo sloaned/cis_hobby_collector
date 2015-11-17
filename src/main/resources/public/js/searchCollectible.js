7ceac7d5ae32981da5e2f6e0ccca7827b7bc231f
unknown <mollison@PC20141.catalystsolves.com>
2015-11-17 14:04:48 -0800

    initial completion of front-end search

--- src/main/resources/public/js/searchCollectible.js
+++ src/main/resources/public/js/searchCollectible.js
@@ -1,45 +1,56 @@
 var collectibles;
 function callSearch(){
-    	alert();
-    	var search = {};
-    	search.category = $("#collectibleSearch").val();
-        search.color = $("#colorSearch").val(); 
-        search.condition = $("#conditionSearch").val();
-        search.age = $("#eraSearch").val();
-        search.description = $("#descriptionSearch").val();
-        search.name = $("#nameSearch").val();
-        search.keyword = $("#keywordsSearch").val();
-        search.sold = $("#soldSearch").val();
-        search.catalogNumber = $("#catalogNumberSearch").val();
-        alert(search);
+    	alert("callSearch");
+    	var search ={};
+    	search.category = $("#collectibleSearch").val().toLowerCase();
+        search.color = $("#colorSearch").val().toLowerCase(); 
+        search.condition = $("#conditionSearch").val().toLowerCase();
+        search.age = $("#eraSearch").val().toLowerCase();
+        search.description = $("#descriptionSearch").val().toLowerCase();
+        search.name = $("#nameSearch").val().toLowerCase();
+        search.keyword = $("#keywordsSearch").val().toLowerCase();
+        var sold = $("#soldSearch").val().toLowerCase();
+        if(sold=="true"){
+        	search.sold=true;
+        }
+        else if(sold=="false"){
+        	search.sold=false;
+        }
+        else{
+        	search.sold=null;
+        }
+        search.catalogNumber = $("#catalogNumberSearch").val().toLowerCase();
+        alert("checking");
+        alert("mick"+search.color);
     	$.ajax({
-        url: '/collectibles/{search}',
+        url: '/collectibles/search',
         method: 'POST',
         contentType: 'application/json',
         data: JSON.stringify(search)
     }).then(function(searchResult){
     	collectibles = searchResult;
     	alert("s");
+    	replaceTable();
         console.log("Post successful")
-        location.reload(true);
     }, function(error){
+    	replaceTable();
     	alert("f");
         console.log(error);
     });
+    	$("[data-toggle='popover']").popover('toggle');
     }
-function replaceTable(e){
+function replaceTable(){
 	alert("remove");
 	 $("tbody").children().remove();
      for (var i = 0; i < collectibles.length; i++) {
-         addDataToRow(collectibles[i]);
+         addRow(collectibles[i]);
      }
-	 e.preventDefault();
 }
 
-function addDataToRow(collectible){
+function addRow(collectible){
 	var row = "<tr><td><div>" + capitalizeWord(collectible.category.category) + "</div></td><td><div>"
 			+ capitalizeWord(collectible.color.color) + "</div></td><td><div>" + capitalizeWord(collectible.condition.condition) + "</div></td><td><div>"
-			+ capitalizeWord(collectible.age.age) + "</div></td><td title=\"" + capitalizeWord(collectible.description) + "\"><div>" + capitalizeWord(collectible.description).truncString(10) + "</div></td><td><div>"
+			+ capitalizeWord(collectible.age.age) + "</div></td><td title=\"" + capitalizeWord(collectible.description) + "\"><div>" + capitalizeWord(collectible.description)+ "</div></td><td><div>"
 			+ capitalizeWord(collectible.name) + "</div></td><td><div>";
 	for (var i = 0; i < collectible.keywords.length; i++) {
 		if (i === collectible.keywords.length - 1)
@@ -53,51 +64,59 @@ function addDataToRow(collectible){
 			+ "<button class='editButton btn btn-default'>Edit</button>" + "</div></td></tr>";
 
     $("tbody").append(row);
-    }
-	
+}
+
 $(document).ready(function(){
 	
-var content = '<form id="searchForm" onsubmit="replaceTable()" class="form-inline" role="form">\
-    <div class="form-group">\
+var content = '<form class="container" id="searchForm" class="form-inline" role="form">\
+  <div class="row">\
+	<div class="form-group col-sm-4">\
       <label>Collectible:</label>\
       <input class="form-control" id="collectibleSearch" placeholder="collectible">\
     </div>\
-    <div class="form-group">\
+    <div class="form-group col-sm-4">\
       <label>Color:</label>\
       <input type="text" class="form-control" id="colorSearch" placeholder="color">\
     </div>\
-    <div class="form-group">\
+    <div class="form-group col-sm-4">\
       <label>Condition:</label>\
       <input type="text" class="form-control" id="conditionSearch" placeholder="condition">\
     </div>\
-    <div class="form-group">\
+  </div>\
+  <div class="row">\
+    <div class="form-group col-sm-4">\
       <label>Era:</label>\
       <input type="text" class="form-control" id="eraSearch" placeholder="era">\
     </div>\
-    <div class="form-group">\
+    <div class="form-group col-sm-4">\
       <label>Name:</label>\
       <input type="password" class="form-control" id="nameSearch" placeholder="name">\
     </div>\
-    <div class="form-group">\
+    <div class="form-group col-sm-4">\
       <label>Keywords:</label>\
       <input type="password" class="form-control" id="keywordsSearch" placeholder="keyword">\
     </div>\
-     <div class="form-group">\
+  </div>\
+  <div class="row">\
+     <div class="form-group col-sm-4">\
       <label>Catalog Number:</label>\
       <input type="text" class="form-control" id="catalogNumberSearch" placeholder="catalog number">\
     </div>\
-	<div class="form-group">\
+	<div class="form-group col-sm-4">\
       <label>Description:</label>\
       <input type="text" class="form-control" id="descriptionSearch" placeholder="description">\
-  </div>\
-   <select id="soldSearch">\
+     </div>\
+     <select id="soldSearch" class="col-sm-4">\
   		<option value="select">Select</option>\
   		<option value="sold">Sold</option>\
   		<option value="notsold">Not Sold</option>\
 	</select>\
-    <button type="submit" id="submitSearch" onclick="callSearch();" class="btn btn-default">Submit</button>\
-  </form>';
+  </div>\
+  <div class="row">\
+    <button type="button" id="submitSearch" onclick="callSearch();" class="btn btn-default">Submit</button>\
+ </div>\
+</form>';
 
-    $('#searchButton').popover({container: 'body',title: "<h3 style='text-align:center'>Collectibles Search</h3>", 
+    $('#searchButton').popover({container: 'body',title: "<h3 style='text-align:center'>Collectibles Search</h3><span class='close'>&times;</span>", 
     content: content, html: true, placement: "top"});  
 });
